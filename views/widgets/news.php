287e81c0b242d7435727a3cf7b8db8d6cd23fb9b
Jeremy Moseley <jeremymoseley@me.com>
2020-10-14 22:27:19 -0700

    NBA Name, other FullName

--- views/widgets/news.php
+++ views/widgets/news.php
@@ -66,7 +66,7 @@
 
                             $teamName  = $team->Name;
                             $teamCity  = $team->City;
-                            $teamFName = ($league[$cat] === 'NFL') ? $team->FullName : $team->Name;
+                            $teamFName = ($league[$cat] === 'NBA') ? $team->Name : $team->FullName;
                             $teamLogo  = $team->WikipediaLogoUrl;
                             $teamColor = $team->PrimaryColor;
 
