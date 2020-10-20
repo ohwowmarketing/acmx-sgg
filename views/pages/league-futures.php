f7e33746c078cb838728b9af8342598c83f38660
Jeremy Moseley <jeremymoseley@me.com>
2020-10-20 09:10:52 -0700

    Futures fixes

--- views/pages/league-futures.php
+++ views/pages/league-futures.php
@@ -10,7 +10,7 @@ switch ( $post->post_parent ) {
     <div class="uk-container uk-container-xlarge">
         <div class="uk-grid-small" uk-grid>
             <div class="uk-width-expand@l">    
-                <div class="uk-card uk-card-default uk-card-body" data-card="future-odds">
+                <div class="uk-card uk-card-default uk-card-body" data-card="futures-widget">
                     <div class="uk-flex uk-flex-between uk-flex-middle">
                         <h1 class="uk-card-title"><?php echo get_the_title( $post->post_parent ) . ' Futures'; ?></h1>
                         <div id="select-loading" style="text-align: right; margin-top: 15px;">
@@ -30,13 +30,41 @@ switch ( $post->post_parent ) {
                     </div>
                 </div>
             </div>
-
-            <div class="uk-width-1-1 uk-width-large@l">
-                <?php
-                get_template_part( widget.'news' );
-                get_template_part( widget.'guides' );
-                ?>
-            </div>
         </div>
     </div>
 </main>
+<!-- <main id="main" class="main" role="main">
+    <div class="uk-container uk-container-xlarge">
+        <div class="uk-container uk-container-xlarge">
+            <div class="uk-card uk-card-default uk-card-body" data-card="odds-ats">
+                <div class="uk-flex uk-flex-between uk-flex-middle">
+                    <h1 class="uk-card-title"><?php echo get_the_title( $post->post_parent ) . ' Futures'; ?></h1>
+                </div>
+                <div class="uk-position-relative">
+                    <div id="table-loading" style="text-align: center; margin-top: 15px;">
+                        <img src="<?php echo get_template_directory_uri(); ?>/resources/images/ui/loading.gif" />
+                    </div>
+                    <div class="uk-overflow-auto">
+                        <table id="ats-table" class="uk-table uk-table-divider" data-league="<?php echo $league; ?>" style="display: none;">
+                            <thead>
+                                <tr>
+                                    <th class="team-label">Team</th>
+                                    <th>Overall</th>
+                                    <?php if ( $league !== 'nfl' ) : ?>
+                                    <th>Home</th>
+                                    <th>Away</th>
+                                    <?php endif; ?>
+                                    <th><small>Last 5 Games</small> ATS Home</th>
+                                    <th><small>Last 5 Games</small> ATS Away</th>
+                                    <th><small>Last 5 Games</small> OV/UN Home</th>
+                                    <th><small>Last 5 Games</small> OV/UN Away</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </div>
+</main> -->
