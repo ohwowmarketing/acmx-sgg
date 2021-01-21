2f929893caf1eb061f7d539d557119d12b0d7543
Jeremy Moseley <jeremymoseley@me.com>
2021-01-21 07:40:24 -0800

    DraftKings promo leaderboard on home page

--- views/pages/home.php
+++ views/pages/home.php
@@ -2,6 +2,9 @@
     <div class="uk-container uk-container-xlarge">
         <div class="uk-grid-small" uk-grid>
             <div class="uk-width-expand@l">
+                <div class="uk-card uk-card-default uk-card-body" data-card="leaderboard">
+                    <img src="<?php echo get_template_directory_uri(); ?>/resources/images/sportsbooks/dk-leaderboard.gif" style="width: 100%" alt="Draft Kings Sportsbook Promotion" />
+                </div>
                 <?php get_template_part( widget . 'oddslines' ); ?>
                 <?php do_action('sportsbook_promos'); ?>
                 <div class="uk-card uk-card-default uk-card-body" data-card="content">
