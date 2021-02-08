40d727aa97599083bc82bf0c8c13428c146fbc47
Jeremy Moseley <jeremymoseley@me.com>
2021-02-07 19:22:19 -0800

    Update NFL Playoff button copy

--- views/fragments/promo.php
+++ views/fragments/promo.php
@@ -5,13 +5,13 @@
         <a href="https://sggplayoffs.com" style="font-family: 'PassionOne-Regular'; font-size: 36px; color: #00A305; line-height: 0.9; margin: 0; text-align: center;">
           2021 NFL Bracket Challenge
         </a><br />
-        <a href="https://sggplayoffs.com" role="button" type="button" class="uk-button uk-button-primarys uk-visible@m" style="margin-top: 5px; padding: 2px 10px; border-radius: 3px; background-color: #00A305; color: #FFF">Sign In | Check Leaderboard</a>
+        <a href="https://sggplayoffs.com" role="button" type="button" class="uk-button uk-button-primarys uk-visible@m" style="margin-top: 5px; padding: 2px 10px; border-radius: 3px; background-color: #00A305; color: #FFF">View Results</a>
       </div>
       <div class="uk-width-1-1 uk-hidden@m" style="text-align: center; margin: 0 auto; padding: 10px 0">
           <a href="https://sggplayoffs.com" style="font-family: 'PassionOne-Regular'; font-size: 28px; color: #00A305; line-height: 0.9; margin: 0 auto; text-align: center;">
             2021 NFL Bracket Challenge
           </a><br />
-          <a href="https://sggplayoffs.com" role="button" type="button" class="uk-button uk-button-primarys" style="margin-top: 5px; padding: 5px 10px; border-radius: 3px; background-color: #00A305; color: #FFF; font-size: .7rem; line-height: .9rem;">Sign In | Check Leaderboard</a>
+          <a href="https://sggplayoffs.com" role="button" type="button" class="uk-button uk-button-primarys" style="margin-top: 5px; padding: 5px 10px; border-radius: 3px; background-color: #00A305; color: #FFF; font-size: .7rem; line-height: .9rem;">View Results</a>
       </div>
     </div>
   </div>
