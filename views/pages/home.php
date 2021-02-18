9cad27ba1a64e3040bfebf2f9072ee97e6024f60
Jeremy Moseley <jeremymoseley@me.com>
2021-02-18 14:43:35 -0800

    Replace promo leaderboard

--- views/pages/home.php
+++ views/pages/home.php
@@ -3,8 +3,8 @@
         <div class="uk-grid-small" uk-grid>
             <div class="uk-width-expand@l">
                 <div class="uk-card uk-card-default uk-card-body" data-card="leaderboard">
-                    <a href="https://sportsbook.draftkings.com/1000-deposit-bonus?referrer=singular_click_id%3d80c5de1c-7e32-4549-b81f-faf88a0c2eb5&wpcid=124180&wpcn=1000DB&wpcrid=&wpsrc=Sports+Gambling+Guides">
-                        <img src="<?php echo get_template_directory_uri(); ?>/resources/images/sportsbooks/dk-leaderboard.gif" style="width: 100%; border: none;" alt="Draft Kings Sportsbook Promotion" />
+                    <a href="https://dksb.sng.link/As9kz/4w3b?_dl=https%3A%2F%2Fsportsbook.draftkings.com%2Fgateway%3Fs%3D517197608%26wpcid%3D129760%26wpcn%3DNBACBB_Bet1Win100%26wpsrc%3DSports%2520Gambling%2520Guides%26wpcrid%3D&pcid=129760">
+                        <img src="<?php echo get_template_directory_uri(); ?>/resources/images/sportsbooks/dk-leaderboard-1.gif" style="width: 100%; border: none;" alt="Draft Kings Sportsbook Promotion" />
                     </a>
                 </div>
                 <?php get_template_part( widget . 'oddslines' ); ?>
