ae81d67e42b93b9ecdc3113b29ae7f74fa78510c
Jeremy Moseley <jeremymoseley@me.com>
2021-03-11 13:21:12 -0800

    DraftKings promo update

--- views/pages/home.php
+++ views/pages/home.php
@@ -4,7 +4,7 @@
             <div class="uk-width-expand@l">
                 <div class="uk-card uk-card-default uk-card-body" data-card="leaderboard">
                     <a href="https://dksb.sng.link/As9kz/4w3b?_dl=https%3A%2F%2Fsportsbook.draftkings.com%2Fgateway%3Fs%3D517197608%26wpcid%3D129760%26wpcn%3DNBACBB_Bet1Win100%26wpsrc%3DSports%2520Gambling%2520Guides%26wpcrid%3D&pcid=129760">
-                        <img src="<?php echo get_template_directory_uri(); ?>/resources/images/sportsbooks/dk-leaderboard-1.gif" style="width: 100%; border: none;" alt="Draft Kings Sportsbook Promotion" />
+                        <img src="<?php echo get_template_directory_uri(); ?>/resources/images/sportsbooks/dk-leaderboard-2.gif" style="width: 100%; border: none;" alt="Draft Kings Sportsbook Promotion" />
                     </a>
                 </div>
                 <?php get_template_part( widget . 'oddslines' ); ?>
