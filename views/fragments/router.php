b7c0ec1dca516a68d4d98050e0c501e868fcee71
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-05-04 06:40:13 +0800

    Update router IG Feed position

--- views/fragments/router.php
+++ views/fragments/router.php
@@ -1,15 +1,14 @@
 <div id="newsletter" class="uk-background-muted uk-padding uk-light" data-globals="newsletter">
-    <div class="uk-container uk-container-small">
-
+    <div class="uk-container">
         <div class="uk-panel uk-margin-medium-bottom">
             <?php echo do_shortcode( '[elfsight_instagram_feed id="1"]' ); ?>
-        </div>
+        </div>        
+    </div>
 
+    <div class="uk-container uk-container-small">
         <div class="uk-text-center">
             <h3>Join Our Mailing List</h3>
             <?php echo do_shortcode( '[wpforms id="236" title="false" description="false"]' ); ?>
         </div>
-
-
     </div>
 </div>
