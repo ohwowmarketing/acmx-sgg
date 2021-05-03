290fbdb109890daff0faa9b2d7ee9c0c6a7283ac
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-05-04 06:37:09 +0800

    Add IG Feed

--- views/fragments/router.php
+++ views/fragments/router.php
@@ -1,6 +1,9 @@
 <div id="newsletter" class="uk-background-muted uk-padding uk-light" data-globals="newsletter">
     <div class="uk-container uk-container-small">
 
+        <div class="uk-panel">
+            <?php echo do_shortcode( '[elfsight_instagram_feed id="1"]' ); ?>
+        </div>
 
         <div class="uk-text-center">
             <h3>Join Our Mailing List</h3>
