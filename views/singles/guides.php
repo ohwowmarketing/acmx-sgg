dbe458ae1ac53bf1a715058b2cef87a47d09e9f0
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-06-17 06:09:10 +0800

    Minor update on Guides content

--- views/singles/guides.php
+++ views/singles/guides.php
@@ -2,14 +2,13 @@
     <div class="uk-container uk-container-xlarge">
         <div class="uk-grid-small" uk-grid>
             <div class="uk-width-expand@l">
-                <div id="guide-content" class="uk-card uk-card-default uk-card-body" data-card="content">
+                <div id="guide-content" class="uk-card uk-card-default uk-card-body" data-card="guides">
                     <article class="uk-article uk-margin-bottom">
                     <?php
                         if ( has_post_thumbnail() ) {
                             the_post_thumbnail();
+                            echo '<div class="uk-text-right uk-text-meta">'.get_the_post_thumbnail_caption().'</div>';
                         }
-
-                        echo '<small class="uk-text-meta uk-align-right uk-margin-top">'.get_the_post_thumbnail_caption().'</small>';
 
                         the_title('<h2 class="uk-article-title">','</h2>');
                         the_content();
