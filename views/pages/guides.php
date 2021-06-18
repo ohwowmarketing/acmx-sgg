8967d90a16dd94dca9ae0247d65509c6c464d537
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-06-19 02:41:46 +0800

    Updates on Guides & Skip to Article button

--- views/pages/guides.php
+++ views/pages/guides.php
@@ -3,24 +3,27 @@
         <div class="uk-grid-small" uk-grid>
 
             <div class="uk-width-expand@l">
-                <div class="uk-card uk-card-default uk-card-body" data-card="guides">
+                <div class="uk-card uk-card-default uk-card-body" data-card="guides-full">
                     <h1 class="uk-card-title"><?php the_title(); ?></h1>
 
                     <?php $guides = ['post_type'=>'sports_guides','post_status'=>'publish','has_password'=>false,'posts_per_page'=>-1,'orderby'=>'menu_order','order'=>'ASC'];
                     query_posts( $guides ); ?>
-                    <div uk-grid class="uk-grid-match uk-child-width-1-2@s uk-child-width-1-3@xl" uk-height-match="target: > div > article > h3">
-                        <?php while ( have_posts() ) : the_post(); ?>
-                        <div class="article-guides">
-                            <article class="uk-article">
-                                <?php if ( has_post_thumbnail() ) {
-                                    echo '<a href="'. get_permalink() .'">'. get_the_post_thumbnail() .'</a>';
-                                } ?>                                
-                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                            </article>
+                    <div uk-grid>
+                      <?php while ( have_posts() ) : the_post(); ?>
+                        <div class="uk-width-1-2@s uk-width-1-3@m">
+                          <figure>
+                              <a href="<?php the_permalink(); ?>">
+                                <?php if ( has_post_thumbnail() ) : ?>
+                                 <?php echo wp_get_attachment_image( get_post_thumbnail_id(), [ 640, 360, true ] ); ?>
+                                <?php else : ?>
+                                  <?php the_title(); ?>
+                                <?php endif; ?>
+                              </a>
+                          </figure>
+                          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                         </div>
-                        <?php endwhile; wp_reset_query(); ?>
+                      <?php endwhile; wp_reset_query(); ?>
                     </div>
-
                 </div>
             </div>
 
