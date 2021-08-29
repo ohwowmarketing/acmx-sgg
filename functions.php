d0191c719e6c58ee527f4beab5d4fbe1b87d9000
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-08-29 08:12:28 -0700

    Check for RSS Feed

--- functions.php
+++ functions.php
@@ -30,6 +30,17 @@ foreach ( $fn_config as $config ) {
 }
 unset($config, $files);
 
+function rss_post_thumbnail($content) {
+    global $post;
+    if(has_post_thumbnail($post->ID)) {
+    $content = '<p>' . get_the_post_thumbnail($post->ID) .
+    '</p>' . get_the_content();
+    }
+    return $content;
+    }
+    add_filter('the_excerpt_rss', 'rss_post_thumbnail');
+    add_filter('the_content_feed', 'rss_post_thumbnail');
+
 //!
 //! Global Definitions
 //!
