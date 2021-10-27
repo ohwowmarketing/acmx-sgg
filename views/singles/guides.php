223773cf641394bb6dfb425cdd43722910d0fb7b
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-10-26 17:19:38 -0700

    Integrate header video to Guides

--- views/singles/guides.php
+++ views/singles/guides.php
@@ -5,10 +5,28 @@
                 <div id="guide-content" class="uk-card uk-card-default uk-card-body" data-card="guides">
                     <article class="uk-article uk-margin-bottom">
                     <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail();
-                            $description = get_post(get_post_thumbnail_id())->post_excerpt;
-                            echo '<div class="uk-text-meta">'. makeUrltoLink($description) .'</div>';
+                        $videoHDR = get_field( 'activate_videohdr' );
+
+                        if ( $videoHDR ) {
+                            
+                            // the_field( 'embed_videohdr' );
+
+                            $url = get_field( 'embed_videohdr' );
+                            parse_str( parse_url( $url, PHP_URL_QUERY ), $my_array_of_vars );
+                            // echo $my_array_of_vars['v'];
+
+                            echo '<div class="iframe-container">';
+                            echo '<iframe src="https://www.youtube-nocookie.com/embed/'.$my_array_of_vars["v"].'?autoplay=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=0" frameborder="0" allowfullscreen uk-responsive uk-video="automute: false"></iframe>';
+                            echo '</div>';
+
+                        } else {
+
+                            if ( has_post_thumbnail() ) {
+                                the_post_thumbnail();
+                                $description = get_post(get_post_thumbnail_id())->post_excerpt;
+                                echo '<div class="uk-text-meta">'. makeUrltoLink($description) .'</div>';
+                            }                            
+                            
                         }
 
                         the_title('<h2 class="uk-article-title">','</h2>'); 
