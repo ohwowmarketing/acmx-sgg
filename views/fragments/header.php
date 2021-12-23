ee65d90514c4f351ca1b81898112c1289df112f1
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-12-22 18:24:32 -0800

    Exclude promos from Cappers landing pages

--- views/fragments/header.php
+++ views/fragments/header.php
@@ -75,7 +75,7 @@
                 </div>
             </div>
             <div class="sb-reviews-link">
-                <?php if ( ! is_page([ 2053, 2168, 2166, 2178 ]) && ! is_singular( 'cappers_corner' ) ) { // Do not show
+                <?php if ( ! is_page([ 2053, 2168, 2166, 2178, 2174, 2182, 2172 ]) && ! is_singular( 'cappers_corner' ) ) { // Do not show
                     do_action( 'sportsbook_daily_promos' );
                 } ?>
             </div>
