8aa970869c57b6c50dfa0779bf08ecd8118605e4
Mark Joseph Rivera <imarkdesigns@gmail.com>
2021-12-28 22:19:46 -0800

    Minor update on Sub-Affiliate Agreement

--- views/pages/agreement.php
+++ views/pages/agreement.php
@@ -1,7 +1,8 @@
 <main id="main" class="main" role="main">
-    <section class="uk-section">
+    <section class="uk-section uk-section-small">
         <div class="uk-container uk-container-small">
-            <div class="uk-card uk-card-default uk-card-body" data-card="cappers-corner">
+            <div class="uk-card uk-card-default uk-card-body" data-card="affiliate-agreement">
+                <h3 class="uk-card-title"><?php the_title(); ?></h3>
                 <?php echo do_shortcode('[forminator_form id="2642"]'); ?>
             </div>
         </div>
